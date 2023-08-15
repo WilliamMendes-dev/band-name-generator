1a27eb98c6e0d1395428711b8b2663e7ce9189dd
William Mendes <115948885+WilliamMendes-dev@users.noreply.github.com>
2023-08-14 21:09:26 -0300

    Create main.py
    Arquivo único do programa.

---
+++ main.py
@@ -0,0 +1,9 @@
+#1. Introdução para o programa.
+print("Bem vindo ao Super Gerador de Nomes de Bandas!")
+#2. Perguntando o nome da cidade natal do usuário.
+cidade = input("Qual é o nome da cidade que você nasceu?\n")
+#3. Perguntando o nome do primeiro pet do usuário
+pet = input("Qual é o nome do seu primeiro pet?\n")
+#4. Combinando os nomes para gerar um novo nome para a banda.
+print("O nome da sua banda pode ser " + cidade + " " + pet + "!\n")
+print("Eai curtiu o nome? haha")
